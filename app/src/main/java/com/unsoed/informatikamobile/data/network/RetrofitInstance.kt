c3b5a41dce6dd0a2bf0e83102a9211817b859648
FawwazFatchurr <fawwazfathur01@gmail.com>
2025-09-23 19:21:43 +0700

    tugas pert 3

---
+++ app/src/main/java/com/unsoed/informatikamobile/data/network/RetrofitInstance.kt
@@ -0,0 +1,16 @@
+package com.unsoed.informatikamobile.data.network
+
+import com.unsoed.informatikamobile.utils.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    val api: OpenLibraryApi by lazy{
+        Retrofit.Builder()
+            .baseUrl( Constants.Base_URL)
+            .addConverterFactory( GsonConverterFactory.create())
+            .build()
+            .create(OpenLibraryApi::class.java)
+
+    }
+}
